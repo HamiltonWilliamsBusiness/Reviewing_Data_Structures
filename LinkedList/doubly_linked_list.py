595e4c97d791af0d68e8fbb2da44ffe81911f97a
Hamilton Williams <hamiltonwilliamsbusiness@gmail.com>
2024-02-01 20:32:47 -0600

    Feat: Setup the problem for solving

--- LinkedList/doubly_linked_list.py
+++ LinkedList/doubly_linked_list.py
@@ -10,8 +10,40 @@ class DoublyLinkedList:
         self.tail = None
         self.count = 0
     
-# Define the order to implement functions next time! Just leave 
-# Do it my leaving the function declarations and nothing else!
+    # Define the order to implement functions next time! Just leave 
+    # Do it my leaving the function declarations and nothing else!
+        
+    def print_forward(self):
+        # This method prints list in forward direction. Use node.next
+        pass
 
+    def print_backward(self):
+        # Print linked list in reverse direction. Use node.prev for this.
+        pass
 
-    
+    def get_length(self):
+        pass
+
+    def insert_at_begining(self, data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def insert_at(self, index, data):
+        pass
+
+    def remove_at(self, index):
+        pass
+
+    def insert_values(self, data_list):
+        pass
+
+    def insert_after_value(self, data_after, data_to_insert):
+        pass
+
+    def remove_by_value(self, data):
+        pass
+
+if __name__ == '__main__':
+    pass
